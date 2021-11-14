17c7e1908f4589d76861ca6bf9ea2acb9ddc1c89
saisona <alexandre.saison.pro@gmail.com>
2021-11-14 23:09:37 +0100

    Update: call basic handler for db

--- cmd/main.go
+++ cmd/main.go
@@ -13,6 +13,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -26,6 +27,7 @@ func main() {
 	//datadogClient := *datadog.NewAPIClient(&datadog.Configuration{Host: "datadoghq.eu", Debug: true})
 
 	config, err := simba.InitConfig()
+	dbClient := simba.InitDbClient(config.DB.Host, config.DB.Username, config.DB.Password, config.DB.Name)
 	if err != nil {
 		log.Fatalf("Failed launching server: %s", err.Error())
 	}
@@ -61,8 +63,7 @@ func main() {
 		err := json.Unmarshal([]byte(c.Request().FormValue("payload")), &callBackStruct)
 		if err != nil {
 			return err
-		}
-		if len(callBackStruct.ActionCallback.AttachmentActions) > 0 {
+		} else if len(callBackStruct.ActionCallback.AttachmentActions) > 0 {
 			blockAttachmentActions := callBackStruct.ActionCallback.AttachmentActions
 			log.Printf("There is %d block actions", len(blockAttachmentActions))
 			for idx, action := range blockAttachmentActions {
@@ -72,16 +73,30 @@ func main() {
 		} else if len(callBackStruct.ActionCallback.BlockActions) > 0 {
 			blockActions := callBackStruct.ActionCallback.BlockActions
 			user := callBackStruct.User
+			channelId := callBackStruct.Channel.ID
 			userId := user.ID
 			profile, err := slackClient.GetUserProfile(&slack.GetUserProfileParameters{UserID: userId})
 			if err != nil {
-				log.Printf("Warning some error while fetchingProfile = %s ", err.Error())
+				log.Printf("Warning some error while fetchingProfile:  %s ", err.Error())
 			}
 
 			userName := profile.DisplayName
-			log.Printf("There is %d block actions", len(blockActions))
+			teamName := profile.Title
+			members := callBackStruct.Channel.Members
+			log.Printf("Team=%s and Members of the channel are => %v", teamName, members)
+
 			for _, action := range blockActions {
-				log.Printf("User (Id: %s) %s clicked on %s", userId, userName, action.Value)
+				log.Printf("User (Id:%s) %s clicked on %s", userId, userName, action.Value)
+				if !strings.Contains(action.Value, "mood") {
+					log.Printf("Warning this has to be handled by another thing (value:%s)", action.Value)
+				} else if err := simba.HandleAddDailyMood(dbClient, channelId, userId, userName, action.Value, ""); err != nil {
+					return err
+				} else {
+					log.Printf("Mood %s has been added for the daily for %s", action.Value, userName)
+					simba.SendSlackTSMessage(slackClient, config, fmt.Sprintf("<@%s> has responded to the daily message with %s", userId, action.Value), action.ActionTs)
+					slackClient.AddReaction("heart", slack.ItemRef{Timestamp: action.ActionTs, Channel: channelId})
+					return nil
+				}
 			}
 
 		} else {
